dadb3a3b827c0850fb5e83b8df376bd7ce374d98
Guilherme Pottratz <145717473+Guipo1@users.noreply.github.com>
2025-05-08 15:58:53 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+    public static void main(String[] args) {
+        Calculador calcular = new Calculador();
+        System.out.println("Ola, seja bem vindo");
+        calcular.calcularMedia();
+    }
+}
